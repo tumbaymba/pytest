1833bb927488946c435d6aaadd23983e71676a8c
tumbaymba <54357075+tumbaymba@users.noreply.github.com>
2023-12-06 09:49:33 +0300

    Update test_arrs.py
    Correct last line

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -15,4 +15,4 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], 10) == []
     assert arrs.my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
     assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4, 5], -10) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], -10) == [1, 2, 3, 4, 5]
